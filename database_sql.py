e402cfb73db8c2b9d911d1e28510dd352f535107
n1k1t1n <nikanig@gmail.com>
2015-05-06 19:08:25 +0300

    Update database_sql.py

--- database_sql.py
+++ database_sql.py
@@ -1,6 +1,6 @@
 sql_types = {'int':'INTEGER', 'long':'INTEGER', 'str':'TEXT', 'unicode':'TEXT', 'float':'REAL', 'list':'INTEGER[]'}
 
-from classes import Product, Ad, Category, Image
+from classes import *
 import psycopg2
 import traceback
 
@@ -18,83 +18,73 @@ class database_sql():
         
     def init_tables(self):
         self.cursor = self.db.cursor()
-        try:
-            columns = Category().to_dict()
-            del columns['id']
-            request = 'CREATE TABLE IF NOT EXISTS Category '
-            request += "(id SERIAL UNIQUE"
-            for column, value in columns.items():
-                request += ', ' + column + ' ' + \
-                           sql_types[type(value).__name__]
-            request += ")"
-            self.cursor.execute(request)
-            self.db.commit()
+        
+        columns = Category().to_dict()
+        del columns['id']
+        request = 'CREATE TABLE IF NOT EXISTS Category '
+        request += "(id SERIAL UNIQUE"
+        for column, value in columns.items():
+            request += ', ' + column + ' ' + \
+                        sql_types[type(value).__name__]
+        request += ")"
+        self.cursor.execute(request)
+        self.db.commit()
 
 
-            columns = Image().to_dict()
-            del columns['id']
-            request = 'CREATE TABLE IF NOT EXISTS Image '
-            request += "(id SERIAL UNIQUE, bytes bytea)"
-            self.cursor.execute(request)
-            self.db.commit()
+        columns = Image().to_dict()
+        del columns['id']
+        request = 'CREATE TABLE IF NOT EXISTS Image '
+        request += "(id SERIAL UNIQUE, bytes bytea)"
+        self.cursor.execute(request)
+        self.db.commit()
             
             
-            columns = Ad().to_dict()
-            del columns['id']
-            request = 'CREATE TABLE IF NOT EXISTS Ad '
-            request += "(id SERIAL UNIQUE"
-            for column, value in columns.items():
-                request += ', ' + column + ' ' + \
-                           sql_types[type(value).__name__]
-            request += ")"
-            self.cursor.execute(request)
-            self.db.commit()
+        columns = Ad().to_dict()
+        del columns['id']
+        request = 'CREATE TABLE IF NOT EXISTS Ad '
+        request += "(id SERIAL UNIQUE"
+        for column, value in columns.items():
+            request += ', ' + column + ' ' + \
+                        sql_types[type(value).__name__]
+        request += ")"
+        self.cursor.execute(request)
+        self.db.commit()
             
             
-            columns = Product().to_dict()
-            del columns['id']
-            request = 'CREATE TABLE IF NOT EXISTS Product '
-            request += "(id SERIAL UNIQUE"
-            for column, value in columns.items():
-                request += ', ' + column + ' ' + \
-                           sql_types[type(value).__name__]
-            request += ")"
-            self.cursor.execute(request)
-            self.db.commit()
+        columns = Product().to_dict()
+        del columns['id']
+        request = 'CREATE TABLE IF NOT EXISTS Product '
+        request += "(id SERIAL UNIQUE"
+        for column, value in columns.items():
+            request += ', ' + column + ' ' + \
+                        sql_types[type(value).__name__]
+        request += ")"
+        self.cursor.execute(request)
+        self.db.commit()
             
             
-            columns = Order().to_dict()
-            del columns['id']
-            request = 'CREATE TABLE IF NOT EXISTS Order '
-            request += "(id SERIAL UNIQUE"
-            for column, value in columns.items():
-                request += ', ' + column + ' ' + \
-                           sql_types[type(value).__name__]
-            request += ")"
-            self.cursor.execute(request)
-            self.db.commit()
-            
-        except Exception as e:
-            print e
+        columns = Order().to_dict()
+        del columns['id']
+        request = 'CREATE TABLE IF NOT EXISTS Orders '
+        request += "(id SERIAL UNIQUE"
+        for column, value in columns.items():
+            request += ', ' + column + ' ' + \
+                        sql_types[type(value).__name__]
+        request += ")"
+        self.cursor.execute(request)
+        self.db.commit()
 
     def cleanup_db(self):
         try:
-            self.cursor.execute("DROP TABLE IF EXISTS Order")
+            self.cursor.execute("DROP TABLE IF EXISTS Orders")
             self.cursor.execute("DROP TABLE IF EXISTS Poduct")
             self.cursor.execute("DROP TABLE IF EXISTS Ad")
             self.cursor.execute("DROP TABLE IF EXISTS Image")
             self.cursor.execute("DROP TABLE IF EXISTS Category")
             self.db.commit()
-        except Exception as e:
-            print e
         finally:
             self.db.close()
             
-            
-        
-
-
-
     def add_product(self, product):
         parameters = product.to_dict()
         del parameters['id']
@@ -109,14 +99,11 @@ class database_sql():
         request = request[0:-2]
         request += ')'
         val = ""
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.cursor.execute("SELECT LASTVAL()")
-            val = self.cursor.fetchone()[1]
-            self.db.commit()
-        except Exception as e:
-            print e  
-            return None
+
+        self.cursor.execute(request, parameters.values())
+        self.cursor.execute("SELECT LASTVAL()")
+        val = self.cursor.fetchone()[1]
+        self.db.commit()
         return val
 
     def update_product(self, product):
@@ -129,11 +116,9 @@ class database_sql():
                 parameters[key] = 'ARRAY' + parameters[key]
         request = request[0:-2]
         request += " WHERE id = " + str(product.id)
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.db.commit()
-        except Exception as e:
-            print e
+
+        self.cursor.execute(request, parameters.values())
+        self.db.commit()
         
     def load_product(self, product_id):
         columns = Product().to_dict().keys()
@@ -144,11 +129,9 @@ class database_sql():
         request += ' WHERE id = '
         request += product_id
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
-        except Exception as e:
-            print e
+
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         products = []
         for row in rows:
@@ -167,11 +150,9 @@ class database_sql():
             request += column + ', '
         request = request[0:-2] + ' FROM Product '
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
-        except Exception as e:
-            print e
+
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         products = []
         for row in rows:
@@ -184,12 +165,8 @@ class database_sql():
         return products
 
     def delete_product(self, product_id):
-        try:
-            self.cursor.execute("DELETE from Product where Id = %s" % product_id)
-            self.db.commit()
-            print("Total rows deleted: %s" % self.cursor.rowcount)
-        except Exception as e:
-            print e
+        self.cursor.execute("DELETE from Product where Id = %s" % product_id)
+        self.db.commit()
 
 
 
@@ -203,14 +180,11 @@ class database_sql():
         request = request[0:-2]
         request += ')'
         val = ""
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.cursor.execute("SELECT LASTVAL()")
-            val = self.cursor.fetchone()[1]
-            self.db.commit()
-        except Exception as e:
-            print e  
-            return None
+
+        self.cursor.execute(request, parameters.values())
+        self.cursor.execute("SELECT LASTVAL()")
+        val = self.cursor.fetchone()[1]
+        self.db.commit()
         return val
         
     def update_category(self, category):
@@ -222,11 +196,9 @@ class database_sql():
         
         request = request[0:-2]
         request += " WHERE id = " + str(category.id)
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.db.commit()
-        except Exception as e:
-            print e
+        self.cursor.execute(request, parameters.values())
+        self.db.commit()
+
 
     def load_category_products(self, category_id):
         columns = Product().to_dict().keys()
@@ -237,12 +209,9 @@ class database_sql():
         request += ' WHERE category = '
         request += category_id
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
 
-        except Exception as e:
-            print e
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         products = []
         for row in rows:
@@ -261,12 +230,9 @@ class database_sql():
             request += column + ', '
         request = request[0:-2] + ' FROM Category '
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
 
-        except Exception as e:
-            print e
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         categories = []
         for row in rows:
@@ -287,12 +253,9 @@ class database_sql():
         request += ' WHERE id = '
         request += category_id
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
 
-        except Exception as e:
-            print e
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         categories = []
         for row in rows:
@@ -317,11 +280,9 @@ class database_sql():
             request += count
             request += ' LIMIT '
             request += int(number_of_products)
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
-        except Exception as e:
-            print e
+
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         products = []
         for row in rows:
@@ -345,14 +306,11 @@ class database_sql():
         request = request[0:-2]
         request += ')'
         val = ""
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.cursor.execute("SELECT LASTVAL()")
-            val = self.cursor.fetchone()[1]
-            self.db.commit()
-        except Exception as e:
-            print e  
-            return None
+
+        self.cursor.execute(request, parameters.values())
+        self.cursor.execute("SELECT LASTVAL()")
+        val = self.cursor.fetchone()[1]
+        self.db.commit()
         return val
 
     def update_ad(self, ad):
@@ -363,37 +321,9 @@ class database_sql():
             request += key + '=' + self.f + ', '
         request = request[0:-2]
         request += " WHERE id = " + str(ad.id)
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.db.commit()
-        except Exception as e:
-            print e
 
-    def load_ad_products(self, ad_id):
-        columns = Product().to_dict().keys()
-        request = 'SELECT '
-        for column in columns:
-            request += column + ', '
-        request = request[0:-2] + ' FROM Product '
-        request += ' WHERE ad = '
-        request += ad_id
-        rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
-
-        except Exception as e:
-            print e
-
-        products = []
-        for row in rows:
-            values_dict = {}
-            for i in xrange(len(columns)):
-                values_dict[columns[i]] = row[i]
-            product = Product()
-            product.set_values(values_dict)
-            products.append(product)
-        return products
+        self.cursor.execute(request, parameters.values())
+        self.db.commit()
 
     def load_ads(self):
         columns = Ad().to_dict().keys()
@@ -402,12 +332,10 @@ class database_sql():
             request += column + ', '
         request = request[0:-2] + ' FROM Ad '
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
+        
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
-        except Exception as e:
-            print e
 
         ads = []
         for row in rows:
@@ -428,12 +356,9 @@ class database_sql():
         request += ' WHERE id = '
         request += ad_id
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
-
-        except Exception as e:
-            print e
+        
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         ads = []
         for row in rows:
@@ -446,14 +371,9 @@ class database_sql():
         return ads
 
     def delete_ad(self, ad_id):
-        try:
-            self.cursor.execute("DELETE from Ad where id = %s" % ad_id)
-            self.db.commit()
-            print("Total rows deleted: %s" % self.cursor.rowcount)
-        except Exception as e:
-            print e
-
-
+        self.cursor.execute("DELETE from Ad where id = %s" % ad_id)
+        self.db.commit()
+        
 
     def add_image(self, image):
         parameters = image.to_dict()
@@ -465,14 +385,12 @@ class database_sql():
         request = request[0:-2]
         request += ')'
         val = ""
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.cursor.execute("SELECT LASTVAL()")
-            val = self.cursor.fetchone()[1]
-            self.db.commit()
-        except Exception as e:
-            print e  
-            return None
+
+        self.cursor.execute(request, parameters.values())
+        self.cursor.execute("SELECT LASTVAL()")
+        val = self.cursor.fetchone()[1]
+        self.db.commit()
+            
         return val
 
     def update_image(self, image):
@@ -484,11 +402,8 @@ class database_sql():
         request = request[0:-2]
         request += " WHERE id = " + str(image.id)
 
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.db.commit()
-        except Exception as e:
-            print e
+        self.cursor.execute(request, parameters.values())
+        self.db.commit()
 
     def get_image(self, image_id):
         columns = Image().to_dict().keys()
@@ -497,13 +412,10 @@ class database_sql():
             request += column + ', '
         request = request[0:-2] + ' FROM Image '
         row = []
-        try:
-            self.cursor.execute(request)
-            row = self.cursor.fetchone()
-        except Exception as e:
-            print e
-            row = None
 
+        self.cursor.execute(request)
+        row = self.cursor.fetchone()
+        
         values_dict = {}
         for i in xrange(len(columns)):
             values_dict[columns[i]] = row[i]
@@ -512,20 +424,14 @@ class database_sql():
         return ad
 
     def delete_image(self, image_id):
-        try:
-            self.cursor.execute("DELETE from Image where id = %s" % image_id)
-            self.db.commit()
-            print("Total rows deleted: %s" % self.cursor.rowcount)
-        except Exception as e:
-            print e
-
-
+        self.cursor.execute("DELETE from Image where id = %s" % image_id)
+        self.db.commit()
 
     def add_order(self, order):
         parameters = order.to_dict()
         del parameters['id']
         keys = parameters.keys()
-        request = 'INSERT INTO Order ('
+        request = 'INSERT INTO Orders ('
         for key in keys:
             request += key + '=' + self.f + ', '
             if (type(parameters[key]).__name__=='list'):
@@ -533,31 +439,27 @@ class database_sql():
         request = request[0:-2]
         request += ')'
         val = ""
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.cursor.execute("SELECT LASTVAL()")
-            val = self.cursor.fetchone()[1]
-            self.db.commit()
-        except Exception as e:
-            print e  
-            return None
+
+        self.cursor.execute(request, parameters.values())
+        self.cursor.execute("SELECT LASTVAL()")
+        val = self.cursor.fetchone()[1]
+        self.db.commit()
+        
         return val
 
     def update_order(self, order):
         parameters = order.to_dict()
         keys = parameters.keys()
-        request = 'UPDATE Order SET '
+        request = 'UPDATE Orders SET '
         for key in keys:
             request += key + '=' + self.f + ', '
             if (type(parameters[key]).__name__=='list'):
                 parameters[key] = 'ARRAY' + parameters[key]
         request = request[0:-2]
         request += " WHERE id = " + str(order.id)
-        try:
-            self.cursor.execute(request, parameters.values())
-            self.db.commit()
-        except Exception as e:
-            print e
+    
+        self.cursor.execute(request, parameters.values())
+        self.db.commit()
         
     def load_order_products(self, order_id):
         columns = Product().to_dict().keys()
@@ -565,14 +467,16 @@ class database_sql():
         for column in columns:
             request += column + ', '
         request = request[0:-2] + ' FROM Product '
-        request += ' WHERE order = '
-        request += order_id
+        request += ' WHERE '
+        if len(load_order(order_id).products>0):
+            for i in load_order(order_id).products:
+                request += 'id = ' + i + ' OR '
+            request = request[0:-4]
+        
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
-        except Exception as e:
-            print e
+
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         products = []
         for row in rows:
@@ -589,13 +493,11 @@ class database_sql():
         request = 'SELECT '
         for column in columns:
             request += column + ', '
-        request = request[0:-2] + ' FROM Order '
+        request = request[0:-2] + ' FROM Orders '
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
-        except Exception as e:
-            print e
+        
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         order = []
         for row in rows:
@@ -612,16 +514,13 @@ class database_sql():
         request = 'SELECT '
         for column in columns:
             request += column + ', '
-        request = request[0:-2] + ' FROM Order '
+        request = request[0:-2] + ' FROM Orders '
         request += ' WHERE id = '
         request += order_id
         rows = []
-        try:
-            self.cursor.execute(request)
-            rows = self.cursor.fetchall()
 
-        except Exception as e:
-            print e
+        self.cursor.execute(request)
+        rows = self.cursor.fetchall()
 
         orders = []
         for row in rows:
