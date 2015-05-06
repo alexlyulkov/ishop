f9f10d3545e16ae3da6b5a7ba1f5c0453da3f185
Alex Lyulkov <a@mailgunhq.com>
2015-05-06 13:45:43 +0300

    added all incoming links

--- http_server.py
+++ http_server.py
@@ -54,7 +54,6 @@ def get_js(filename):
 
 
 @http_server.route("/")
-#@website_authorization.requires_auth
 def index_page():
     try:
         return website.index_page()
@@ -99,24 +98,46 @@ def categoty_page(category_id):
         print e
 
 @http_server.route("/cart", methods=['GET', 'POST'])
-@website_authorization.requires_auth
-def cart_page(product_id):
+def cart_page():
     try:
         return website.cart_page()
     except Exception, e:
         print traceback.format_exc()
         print e
 
+@http_server.route("/add_to_cart/<int:product_id>", methods=['GET', 'POST'])
+def add_to_cart(product_id):
+    try:
+        return website.add_to_cart(product_id)
+    except Exception, e:
+        print traceback.format_exc()
+        print e
+
+@http_server.route("/delete_from_cart/<int:product_id>", methods=['GET', 'POST'])
+def delete_from_cart(product_id):
+    try:
+        return website.delete_from_cart(product_id)
+    except Exception, e:
+        print traceback.format_exc()
+        print e
+
+@http_server.route("/ordering", methods=['GET', 'POST'])
+
+
+
+
 @http_server.route("/new_product", methods=['GET', 'POST'])
 @website_authorization.requires_auth
 def add_product():
     try:
-        return render_template('addProduct.html', \
-                               product = Product())
+        return render_template("addProduct.html", product = Product())
 
     except Exception, e:
         print traceback.format_exc()
         print e
+
+@http_server.route("/modify_product/<int:product_id>", methods=['GET', 'POST'])
+@website_authorization.requires_auth
 
 @http_server.route("/save_product", methods=['GET', 'POST'])
 @website_authorization.requires_auth
@@ -126,6 +147,7 @@ def save_product():
         uploaded_files = request.files.getlist("images")
         print uploaded_files
         image_bytes = uploaded_files[0].read()
+        print type(image_bytes)
         return send_file(io.BytesIO(image_bytes),
                          attachment_filename='product_picture.png',
                      mimetype='image/png')
@@ -133,97 +155,27 @@ def save_product():
         print traceback.format_exc()
         print e
 
-
-
-
-
-        
-@http_server.route("/employee/<int:employee_id>", methods=['GET', 'POST'])
+@http_server.route("/new_category", methods=['GET', 'POST'])
 @website_authorization.requires_auth
-def edit_employee_page(employee_id):
-    return website.edit_employee_page(employee_id)
 
-@http_server.route("/new_employee", methods=['GET', 'POST'])
+@http_server.route("/modify_category/<int:category_id>", methods=['GET', 'POST'])
 @website_authorization.requires_auth
-def new_employee_page():
-    try:
-        res =  website.edit_employee_page(-1, new_employee = True)
-        return res
-    except Exception, e:
-        print traceback.format_exc()
-        print e
 
-@http_server.route("/save_employee", methods=['GET', 'POST'])
+@http_server.route("/save_category", methods=['GET', 'POST'])
 @website_authorization.requires_auth
-def save_employee_page():
-    try:
-        values = request.form
-        return website.save_employee(values)
-    except Exception, e:
-        print traceback.format_exc()
-        print e
 
-@http_server.route("/delete_employee", methods=['GET', 'POST'])
+@http_server.route("/orders", methods=['GET', 'POST'])
 @website_authorization.requires_auth
-def delete_employee():
-    try:
-        id = request.form.get('id')
-        return website.delete_employee(id)
-    except Exception, e:
-        print traceback.format_exc()
-        print e
 
-
-@http_server.route("/save_employee_password", methods=['GET', 'POST'])
+@http_server.route("/order/<int:order_id>", methods=['GET', 'POST'])
 @website_authorization.requires_auth
-def save_employee_password():
-    try:
-        id = request.form.get('id')
-        password = request.form.get('password')
-        return website.save_employee_password(id, password)
-    except Exception, e:
-        print traceback.format_exc()
-        print e
 
-
-
-@http_server.route("/employees", methods=['GET', 'POST'])
+@http_server.route("/delete_order/<int:order_id>", methods=['GET', 'POST'])
 @website_authorization.requires_auth
-def employees_page():
-    try:
 
-        month = request.form.get('month') or utils.current_month()
-        print '!!!!!!!!', month
-        sorting = int(request.form.get('sorting') or 1)
-        sorting_dir = int(request.form.get('sorting_dir') or 1)
-        if sorting_dir == 2:
-            sorting = -sorting
-        filters = request.form.to_dict()
-        if 'month' in filters:
-            del filters['month']
-        if 'sorting' in filters:
-            del filters['sorting']
-        if 'sorting_dir' in filters:
-            del filters['sorting_dir']
-        for key in filters.keys():
-            if filters[key].strip() == '':
-                del filters[key]
-        return website.employees_page(filters, month, sorting)
-    except Exception, e:
-        print traceback.format_exc()
-        print e
 
-    
 
-@http_server.route("/add_working_seconds", methods=['POST'])
-def add_working_seconds():
-    try:
-        print request.form.to_dict()
-        employee_id = int(request.form.get('employee_id'))
-        password = request.form.get('password')
-        working_seconds = int(request.form.get('working_seconds'))
-        return api.add_working_seconds(employee_id, working_seconds, password)
-    except Exception, e:
-        print traceback.format_exc()
-        print e
-        return str(e)
+
+@http_server.route("/contatcs", methods=['GET', 'POST'])
+
+@http_server.route("/delivery", methods=['GET', 'POST'])
